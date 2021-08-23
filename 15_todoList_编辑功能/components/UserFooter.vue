2f58c26a1a505ab8315bdfe2e41417abfe314e98
uloon <252946143@qq.com>
2021-08-23 13:44:37 +0800

    todoList编辑功能

---
+++ 15_todoList_编辑功能/components/UserFooter.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="todo-footer">
+    <label>
+      <input type="checkbox" :checked="isALLDone" @change="checkAll"/>
+    </label>
+    <span>
+      <span>已完成{{haveDone}}</span> / 全部{{todos.length}}
+    </span>
+    <button class="btn btn-danger" @click="clearHaveDone">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+import pubsub from 'pubsub-js'
+export default {
+  name:'UserFooter',
+  props:['todos'],
+  computed:{
+    haveDone(){
+      return this.todos.reduce((pre,todo)=>{
+        return pre +(todo.done?1:0);
+      },0)
+    },
+    isALLDone(){
+      return this.haveDone===this.todos.length&& this.haveDone>0;
+    }
+  },
+  methods:{
+    // 全选或取消全选
+    checkAll(e){
+      // 发送checkAllDone消息
+      pubsub.publish('checkAllDone',e.target.checked);
+    },
+
+    // 清除所有已经完成的
+    clearHaveDone(){
+      if(confirm('确定清除吗？')){
+        // 发布clearAllHaveDone消息
+        pubsub.publish('clearAllHaveDone')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  /*footer*/
+  .todo-footer {
+    height: 40px;
+    line-height: 40px;
+    padding-left: 6px;
+    margin-top: 5px;
+  }
+
+  .todo-footer label {
+    display: inline-block;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+
+  .todo-footer label input {
+    position: relative;
+    top: -1px;
+    vertical-align: middle;
+    margin-right: 5px;
+  }
+
+  .todo-footer button {
+    float: right;
+    margin-top: 5px;
+  }
+</style>
---
+++ 15_todoList_编辑功能/components/UserItem.vue
@@ -0,0 +1,115 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" :checked="todoObj.done" @click="change(todoObj.id)"/>
+      <span v-show="!todoObj.isEdit">{{todoObj.title}}</span>
+      <input 
+        type="text" 
+        @blur="handleBlur(todoObj,$event)"
+        :value="todoObj.title" 
+        v-show="todoObj.isEdit" 
+        ref="inputTitle"
+      >
+    </label>
+    <button class="btn btn-danger" :style="this.btnStyle" @click="deleteTodoObjGetId(todoObj.id)">删除</button>
+    <button class="btn btn-edit" @click="handleEdit(todoObj)" v-show="!todoObj.isEdit">编辑</button>
+  </li>
+</template>
+
+<script>
+import pubsub from 'pubsub-js'
+export default {
+  name:'UserItem',
+  props:['todoObj'],
+  data(){
+    return{
+      btnStyle:'display:none'
+    }
+  },
+  methods:{
+    // 改变勾选框
+    change(id){
+      // 点击发布changeDone消息
+      pubsub.publish('changeDone',id)
+    },
+
+    // 删除todo，首先获取id
+    deleteTodoObjGetId(id){
+      if(confirm('确定删除吗？')){
+        // 点击发布deleteTodoObj消息
+        pubsub.publish('deleteTodoObj',id)
+      }
+    },
+
+    // 编辑todo
+    handleEdit(todoObj){
+      todoObj.isEdit=true;
+      this.$nextTick(function(){
+        this.$refs.inputTitle.focus()
+      })
+    },
+
+    // 编辑框失去焦点
+    handleBlur(todoObj,e){
+      todoObj.isEdit=false;
+      if(!e.target.value.trim()) return alert('不能为空');
+      pubsub.publish('changeTitle',[todoObj.id,e.target.value])
+    }
+  },
+  watch:{
+    'todoObj.done':{
+      immediate:true,
+      handler(newValue){
+        if(newValue){
+          this.btnStyle='display:inline-block'
+        }else{
+          this.btnStyle='display:none'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  li {
+    list-style: none;
+    height: 36px;
+    line-height: 36px;
+    padding: 0 5px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  li label {
+    float: left;
+    cursor: pointer;
+  }
+
+  li label li input {
+    vertical-align: middle;
+    margin-right: 6px;
+    position: relative;
+    top: -1px;
+  }
+
+  li button {
+    float: right;
+    margin-top: 3px;
+  }
+
+  li:before {
+    content: initial;
+  }
+
+  li:last-child {
+    border-bottom: none;
+  }
+
+  li:hover{
+    background-color: rgb(173, 173, 173);
+  }
+
+  li:hover .btn-edit{
+    display: inline-block;
+  }
+</style>
